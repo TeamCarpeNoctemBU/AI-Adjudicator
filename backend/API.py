f8104230e782bbe4d2bc3d29d983710004185c26
= <=>
2022-05-28 12:23:58 +0530

    more progress on backend

--- backend/API.py
+++ backend/API.py
@@ -1,12 +1,15 @@
-from sanic import Sanic
-from sanic.response import text
+#performing flask imports
+from flask import Flask,jsonify
+from backbone import *
 
-app = Sanic("MyHelloWorldApp")
 
-@app.get("/")
-async def hello_world(request):
-    return text("Hello, world.")
+app = Flask(__name__) #intance of our flask application 
 
-@app.get("/foo")
-async def foo_handler(request):
-    return text("I said foo!")
+#Route '/' to facilitate get request from our flutter app
+@app.route('/', methods = ['GET'])
+def index():
+    return jsonify({'greetings' : 'Hi! this is python'}) #returning key-value pair in json format
+
+
+if __name__ == "__main__":
+    app.run(debug = True) #debug will allow changes without shutting down the server
