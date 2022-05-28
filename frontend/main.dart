f4a28bbdc39c36a11b0d8d2d52d03ff2d47ddf36
PoyBoy <parvsharma0104@gmail.com>
2022-05-28 07:19:18 +0530

    Buttons

--- frontend/main.dart
+++ frontend/main.dart
@@ -5,7 +5,21 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Death !"),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Ok'),
+        ),
+        body: Column(children: [
+          Text('the questan !'),
+          RaisedButton(child: Text('Ans 1'),onPressed: null,),
+          RaisedButton(child: Text('Ans 2'),onPressed: null,),
+          RaisedButton(child: Text('Ans 3'),onPressed: null,),
+          ],
+        ),
+      ),
+    );
   }
 }
